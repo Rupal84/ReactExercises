065436b492beadfc2ebddee18608878cf70b6d39
Kulwinder_Rupal <Kulwinder_rupal@yahoo.com>
2019-10-20 21:09:48 +1100

    Axios Demo

---
+++ AxiosDemo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AppComp from './AppComp';
+
+function App() {
+  return (
+    <div className="App">
+      <AppComp/>
+    </div>
+  );
+}
+
+export default App;
